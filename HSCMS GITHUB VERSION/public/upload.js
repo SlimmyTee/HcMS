d3205deab6fda1437742053b7453ec70de446fab
DSC Ekiti State University <42493202+DSC-Eksu@users.noreply.github.com>
2021-03-25 16:02:13 +0100

    Update upload.js

--- HSCMS GITHUB VERSION/public/upload.js
+++ HSCMS GITHUB VERSION/public/upload.js
@@ -5,14 +5,13 @@
 
  // Configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyC003a4RDfl_eSBfkEGuZ6Xg4Wr0xQyDJw",
-    authDomain: "practicalwork-bc2b2.firebaseapp.com",
-    databaseURL: "https://practicalwork-bc2b2-default-rtdb.firebaseio.com",
-    projectId: "practicalwork-bc2b2",
-    storageBucket: "practicalwork-bc2b2.appspot.com",
-    messagingSenderId: "933839384927",
-    appId: "1:933839384927:web:c380e4363debfe6e6f86ea",
-    measurementId: "G-3NN5GWF6R7"
+    apiKey: "AIzaSyBeBO_cp1OgwQkppA2ZhzeBmK28B_C-dFw",
+    authDomain: "hcms-86a06.firebaseapp.com",
+    databaseURL: "https://hcms-86a06-default-rtdb.firebaseio.com",
+    projectId: "hcms-86a06",
+    storageBucket: "hcms-86a06.appspot.com",
+    messagingSenderId: "1086929543598",
+    appId: "1:1086929543598:web:5dc094783d5b61a3f8e83f"
   };
 
   //Initialize Firebase
